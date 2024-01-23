2000cf73fcd54fb75ff777d7b61d80e676321a23
tomasdarienko <darienkotomas@gmail.com>
2024-01-23 18:00:27 +0100

    added input section

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="./css/style.css">
+     <title>Document</title>
+</head>
+<body>
+     
+
+<form action="dashboard.php" method="GET ">
+     <input type="text" id="paragrafo" placeholder="paragrafo">
+     <input type="text" id="censura"  placeholder="parola da censurare">
+</form>
+</body>
+</html>
